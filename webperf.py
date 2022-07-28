517bf05857959fab3575a7f0b9ba2ec61a4395c4
cosmicpb <pbaldacimjr@gmail.com>
2022-07-28 06:32:11 -0300

    add status code

--- webperf.py
+++ webperf.py
@@ -11,15 +11,15 @@ from datetime import datetime
 
 @click.command()
 ##@click.option('-u', required=True, type=str)
-@click.option('-m', required=True, type=str)
+@click.option('-d', required=True, type=str)
 @click.option('-s', required=True, type=str)
 @click.option('-k', required=True, type=str)
 
 
 
-def hello(m, s, k):
+def hello(d, s, k):
 
-    with open(m) as data_csv:
+    with open(d) as data_csv:
         csv_data = csv.reader(data_csv)
 
         for row in csv_data:
@@ -34,21 +34,35 @@ def hello(m, s, k):
             r =requests.get('https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=' + row[0] +
             '&strategy=' + s +
             '&key=' + k)
-            responseJson = r.json()
 
-            lcp_Exp = parse('$.lighthouseResult.audits["largest-contentful-paint"].numericValue')
-            lcp = lcp_Exp.find(responseJson)
+            if(r.status_code>400):
 
-            cls_Exp = parse('$.lighthouseResult.audits["cumulative-layout-shift"].numericValue')
-            cls = cls_Exp.find(responseJson)
+                responseJson = r.json()
 
-            tbt_Exp = parse('$.lighthouseResult.audits["total-blocking-time"].numericValue')
-            tbt = tbt_Exp.find(responseJson)
+                message_Exp = parse('$.error.message')
+                msg = message_Exp.find(responseJson)
 
-            a = b + ' ||| ' + str(row[0]) + ' ||| LCP = ' + str(lcp[0].value) + ' ||| CLS = ' + str(cls[0].value) + ' ||| TBT = ' + str(tbt[0].value)
 
-            print(a)
-            
+                c = ' Error. Status Code = ' + str(r.status_code) + '  ' + msg[0].value
+                print(c)
+
+            else:
+
+                responseJson = r.json()
+
+                lcp_Exp = parse('$.lighthouseResult.audits["largest-contentful-paint"].numericValue')
+                lcp = lcp_Exp.find(responseJson)
+
+                cls_Exp = parse('$.lighthouseResult.audits["cumulative-layout-shift"].numericValue')
+                cls = cls_Exp.find(responseJson)
+
+                tbt_Exp = parse('$.lighthouseResult.audits["total-blocking-time"].numericValue')
+                tbt = tbt_Exp.find(responseJson)
+
+                a = b + ' ||| ' + str(row[0]) + ' ||| LCP = ' + str(lcp[0].value) + ' ||| CLS = ' + str(cls[0].value) + ' ||| TBT = ' + str(tbt[0].value)
+
+                print(a)
+                
            
 
  
