4fa17d8e4683e7d5b8002b669d93a8bfa5b9d704
cosmicpb <pbaldacimjr@gmail.com>
2022-07-28 07:35:54 -0300

    add output csv writer

--- webperf.py
+++ webperf.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+from asyncore import write
 import json
 from urllib import response
 from jsonpath_rw import jsonpath, parse
 import click
 import requests
 import csv
+from csv import writer
 from datetime import datetime
 
 
@@ -14,15 +16,19 @@ from datetime import datetime
 @click.option('-d', required=True, type=str)
 @click.option('-s', required=True, type=str)
 @click.option('-k', required=True, type=str)
+@click.option('-o', required=True, type=str)
 
 
 
-def hello(d, s, k):
+def hello(d, s, k, o):
 
+    
+    
     with open(d) as data_csv:
         csv_data = csv.reader(data_csv)
 
         for row in csv_data:
+            ##output = open(o, 'a')
             now = datetime.now()
             a = now.strftime("%d_%m_%Y.%H_%M_%S")
             b = now.strftime("%d_%m_%Y.%H_%M_%S")
@@ -42,9 +48,15 @@ def hello(d, s, k):
                 message_Exp = parse('$.error.message')
                 msg = message_Exp.find(responseJson)
 
-
+                webvitals = [b, str(row[0]), r.status_code, 'ERROR', 'ERROR', 'ERROR']
                 c = ' Error. Status Code = ' + str(r.status_code) + '  ' + msg[0].value
                 print(c)
+                with open(o, 'a', newline='') as output:
+                    wrt = writer(output)
+                    wrt.writerow(webvitals)
+                
+                
+                
 
             else:
 
@@ -59,9 +71,19 @@ def hello(d, s, k):
                 tbt_Exp = parse('$.lighthouseResult.audits["total-blocking-time"].numericValue')
                 tbt = tbt_Exp.find(responseJson)
 
+                webvitals = [b, str(row[0]), r.status_code, str(lcp[0].value), str(cls[0].value), str(tbt[0].value)]
+                with open(o, 'a', newline='') as output:
+                    wrt = writer(output)
+                    wrt.writerow(webvitals)
+                              
+
                 a = b + ' ||| ' + str(row[0]) + ' ||| LCP = ' + str(lcp[0].value) + ' ||| CLS = ' + str(cls[0].value) + ' ||| TBT = ' + str(tbt[0].value)
 
                 print(a)
+
+            
+
+
                 
            
 
